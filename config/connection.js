690271615b0dbdcc03f1f7ab70d922566f240d84
AllieStewart <megaxis1217@gmail.com>
2023-11-20 19:00:01 -0600

    added DB info to /config/connection.js, edited CRUD values in /controllers

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,10 @@
 // Connection using Mongoose.
 const { connect, connection } = require('mongoose');
 
-// After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
-// Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+  process.env.MONGODB_URI || 'mongodb+srv://root:wryipsfhkzcbm@cluster0.72mmbgv.mongodb.net/';
 
 connect(connectionString);
 
 module.exports = connection;
 // End of JS file
-// (Placeholder)
