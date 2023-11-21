5f7662efae66e30f04fe8644391fa3acd63802da
AllieStewart <megaxis1217@gmail.com>
2023-11-21 16:09:29 -0600

    changed mongoDB info

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb+srv://root:wryipsfhkzcbm@cluster0.72mmbgv.mongodb.net/';
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
 
 connect(connectionString);
 
